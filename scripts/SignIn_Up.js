fa01e879dca272b29d7f1cb4d1d54055b7306fe0
Marjan Haghighat <MarjanHD1372@gmail.com>
2025-09-28 19:52:12 +0100

    RegisterHandler added

--- scripts/SignIn_Up.js
+++ scripts/SignIn_Up.js
@@ -1,8 +1,11 @@
 const SignInPage = document.getElementById("SignIn-div");
 const SignUpPage = document.getElementById("SignUp-div");
-
 const SignUpLink = document.getElementById("SignUpHyperLink");
 const SignInLink = document.getElementById("signUpPageLink");
+const UserName_SignUp = document.getElementById("User_SignUp_Input");
+const password_SignUp = document.getElementById("Password_SignUp_input");
+const Email_SignUp = document.getElementById("Email_SignUp_Input");
+const Submit_Registration = document.getElementById("Submit_Registration");
 
 ShowSignUpPageHandler = () => {
   SignInPage.style.transition = "opacity 0.5s ease-in-out";
@@ -25,5 +28,18 @@ ShowSignInPageHandler = () => {
   }, 550);
 };
 
+const RegisterHandler = () => {
+  const userName = UserName_SignUp.value;
+  const password = password_SignUp.value;
+  const email = Email_SignUp.value;
+
+  if (UserName_SignUp === "" && password_SignUp === "") {
+    console.log("no value");
+  } else {
+    console.log(userName, password, email);
+  }
+};
+
 SignUpLink.addEventListener("click", ShowSignUpPageHandler);
 SignInLink.addEventListener("click", ShowSignInPageHandler);
+Submit_Registration.addEventListener("click", RegisterHandler);
