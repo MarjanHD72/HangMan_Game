6c4b1395038bda2110158a0880e0888fe8e5a263
Marjan Haghighat <MarjanHD1372@gmail.com>
2025-09-28 19:10:42 +0100

    some features added

--- scripts/index.js
+++ scripts/index.js
@@ -3,66 +3,18 @@ const SignUpHyperLink = document.getElementById("SignUpHyperLink");
 const SignUp = document.getElementById("SignUp-div");
 const LogIn = document.getElementById("login-div");
 const playBtn = document.getElementById("playBtn");
-// const choices = ["rock", "paper", "scissors"];
-// let playerScore = 0;
-// let computerScore = 0;
-// const checkWinner = (player, computer) => {
-//   if (player === computer) {
-//     return "Even";
-//   } else if (player === "rock") {
-//     return computer === "scissoers" ? "player" : "computer";
-//   } else if (player === "paper") {
-//     return computer === "rock" ? "player" : "computer";
-//   } else if (player === "scissors") {
-//     return computer === "paper" ? "player" : "computer";
-//   } else {
-//     return computer === "rock" ? "computer" : "player";
-//   }
-// };
-// function showResult(result) {
-//   if (result === "player") {
-//     console.log("you win");
-//     playerScore++;
-//   } else if (result === "computer") {
-//     console.log("you lose");
-//     computerScore++;
-//   } else {
-//     console.log("you are even");
-//   }
-//   console.log(`your score: ${playerScore}`);
-//   console.log(`computer score: ${computerScore}`);
-// }
-// const play = () => {
-//   const playerChoice = prompt("please choose rock or paper or scissors");
-//   if (choices.indexOf(playerChoice?.toLowerCase()) !== -1) {
-//     console.log(`You Chose ${playerChoice}`);
-//   } else {
-//     console.log("Don't Cheat");
-//     return;
-//   }
-//   const randomNumber = Math.floor(Math.random() * choices.length);
-//   const computerChoice = choices[randomNumber];
-//   console.log(`computer's choice is: ${computerChoice}`);
-
-//   const gameResult = checkWinner(playerChoice, computerChoice);
-//   showResult(gameResult);
-
-//   if (computerScore === 5) {
-//     console.log("Computer won the Game");
-//   } else if (playerScore === 5) {
-//     console.log("You won the Game");
-//   } else {
-//     play();
-//   }
-// };
-// showResult();
-// play();
 
 function openGamePage() {
   window.location.href = "./HTML/play-Game.html";
 }
 function openRegisterPage() {
-  window.location.href = "./HTML/SignUp.html";
+  const bodyElement = document.body;
+  bodyElement.classList.add("page-fade-out");
+  const transitionDuration = 450;
+  setTimeout(() => {
+    // 4. Perform the redirection after the delay
+    window.location.href = "./HTML/SignUp.html";
+  }, transitionDuration);
 }
 function AccountSwitch() {
   LogIn.style.display = "none";
