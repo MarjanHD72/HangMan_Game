58b3fb6e7dbdd0b0013d9caf72369acb30525821
Marjan Haghighat <MarjanHD1372@gmail.com>
2025-09-29 15:42:36 +0100

    SignIn,SignUp input authentications

--- scripts/SignIn_Up.js
+++ scripts/SignIn_Up.js
@@ -4,9 +4,21 @@ const SignUpLink = document.getElementById("SignUpHyperLink");
 const SignInLink = document.getElementById("signUpPageLink");
 const UserName_SignUp = document.getElementById("User_SignUp_Input");
 const password_SignUp = document.getElementById("Password_SignUp_input");
+const Username_Login = document.getElementById("user_Login-input");
+const Password_Login = document.getElementById("pass_Login-input");
 const Email_SignUp = document.getElementById("Email_SignUp_Input");
 const Submit_Registration = document.getElementById("Submit_Registration");
+const SigninButton = document.getElementById("signInButton");
+const SU_message = document.getElementById("SignUp-message");
+const SignIn_message = document.getElementById("SignIn-message");
 
+//Generate RndomID for Created Users
+const generateId = () => {
+  return Math.round(
+    Math.random() * Math.random() * Math.pow(10, 15)
+  ).toString();
+};
+// Switch Between SigUp and SignIn
 ShowSignUpPageHandler = () => {
   SignInPage.style.transition = "opacity 0.5s ease-in-out";
   SignInPage.style.opacity = 0;
@@ -28,18 +40,53 @@ ShowSignInPageHandler = () => {
   }, 550);
 };
 
+//register New Users on website
+
 const RegisterHandler = () => {
   const userName = UserName_SignUp.value;
   const password = password_SignUp.value;
   const email = Email_SignUp.value;
 
-  if (UserName_SignUp === "" && password_SignUp === "" && Email_SignUp === "") {
-    console.log("Please Enter required Field");
+  if (userName === "" || password === "" || email === "") {
+    showAlert("Please Enter Required Field", "error", SU_message);
   } else {
-    console.log(userName, password, email);
+    showAlert("User added Successfully", "success", SU_message);
+    console.log("hi");
+  }
+};
+// Login to existing account
+const LogInHandler = () => {
+  const userName = Username_Login.value;
+  const password = Password_Login.value;
+  if (userName === "" || password === "") {
+    showAlert("Please Enter Required Field", "error", SignIn_message);
+  } else {
+    showAlert("Logged In Successfully", "success", SignIn_message);
   }
 };
 
+// Alert Message function
+const showAlert = (message, type, targetElement) => {
+  targetElement.innerHTML = "";
+  const alert = document.createElement("p");
+  alert.innerText = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  targetElement.append(alert);
+  setTimeout(() => {
+    if (targetElement === SU_message) {
+      targetElement.innerText = "Create an Account";
+      UserName_SignUp.value = "";
+      password_SignUp.value = "";
+      Email_SignUp.value = "";
+    } else if (targetElement === SignIn_message) {
+      targetElement.innerText = "Log in to Your account";
+      targetElement.style.backgroundColor = "#ecf0f3";
+      Password_Login.value = "";
+    }
+  }, 2000);
+};
 SignUpLink.addEventListener("click", ShowSignUpPageHandler);
 SignInLink.addEventListener("click", ShowSignInPageHandler);
 Submit_Registration.addEventListener("click", RegisterHandler);
+SigninButton.addEventListener("click", LogInHandler);
