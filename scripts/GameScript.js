ec3ccaff01306f47634af7ca9ab7b4623ef015d5
Marjan Haghighat <MarjanHD1372@gmail.com>
2025-10-06 13:58:50 +0100

    Game Improved

--- scripts/GameScript.js
+++ scripts/GameScript.js
@@ -5,6 +5,7 @@ const exitGame = document.getElementById("ExitGame");
 const keyboard = document.getElementById("keyboard");
 const changeBtn = document.getElementById("changeBtn");
 const container = document.getElementById("container");
+
 LoadHeader();
 
 //Exit From Game Page
@@ -103,7 +104,7 @@ var objects = [
   "umbrella",
 ];
 let answers = "";
-let maxWrong = "6";
+const maxWrong = 6;
 let mistakes = 0;
 let guessed = [];
 let WordStatus = null;
@@ -136,13 +137,15 @@ const categoryTasks = {
 //Change Category when clicking on a  button
 
 const ChangeCategoryHandler = () => {
+  message.innerText = "Please Select a Category";
   Category.forEach((button) => {
     button.style.display = "inline-block";
   });
-
-  changeBtn.style.display = "none";
+  container.style.backgroundImage = changeBtn.style.display = "none";
+  document.getElementById("WordLocation").innerHTML = "";
 };
 changeBtn.addEventListener("click", ChangeCategoryHandler);
+
 //selecting Category to do Allocated task
 function SelectedCategoryHandler(event) {
   const task = categoryTasks[event.target.id];
@@ -153,13 +156,85 @@ function SelectedCategoryHandler(event) {
       changeBtn.style.display = "inline-block";
       button.style.display = "none";
     });
-
+    keyboard.style.display = "inline-block";
     guessed = [];
     mistakes = 0;
     WordStatus = null;
-    // GameButton();
+    GameButton();
+    Guess();
   } else {
     alert("select one category");
   }
 }
 ChangeCategoryHandler();
+
+function Guess() {
+  console.log(answers);
+  WordStatus = answers
+    .split("")
+    .map((letter) => (guessed.indexOf(letter) >= 0 ? letter : "_"))
+    .join(" ");
+  document.getElementById("WordLocation").innerHTML = WordStatus;
+}
+Guess();
+
+// Creating Game Buttons when The Category is selected
+function GameButton() {
+  const ButtonsLetter = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(
+      (letter) =>
+        `<button id="${letter}" onclick="GuessHandler('${letter}')">${letter}</button>`
+    )
+    .join("");
+  keyboard.innerHTML = ButtonsLetter;
+
+  document.getElementById("keyboard").innerHTML = ButtonsLetter;
+}
+
+const CheckWinner = () => {
+  const remaining = answers
+    .split("")
+    .filter((letter) => !guessed.includes(letter));
+
+  if (remaining.length === 0) {
+    message.innerText = "🎉Congratulations, You Won!";
+    keyboard.style.display = "none";
+    container.style.backgroundImage = 'url("/Images/Celebrate.gif")';
+    container.style.backgroundSize = "cover"; // کل container پر بشه
+    container.style.backgroundRepeat = "no-repeat"; // تکرار نشه
+    container.style.backgroundPosition = "center"; // وسط تصویر قرار بگیره
+    setTimeout(() => {
+      changeBtn.style.display = "inline-block";
+      container.style.backgroundImage = "none";
+    }, 2000);
+  }
+  // Number(maxWrong)
+  //Case of wrong mistakes and lose Game
+  else if (mistakes >= maxWrong) {
+    message.innerText = "💀 You Lost! The word was: " + answers;
+    container.style.backgroundImage = 'url("/Images/gameOver.gif")';
+    changeBtn.style.display = "none";
+    keyboard.style.display = "none";
+    setTimeout(() => {
+      changeBtn.style.display = "inline-block";
+    }, 2000);
+  }
+};
+// Controling the Guesses of User
+window.GuessHandler = function (letter) {
+  if (guessed.indexOf(letter) === -1) {
+    guessed.push(letter);
+  }
+  if (answers.indexOf(letter) >= 0) {
+    Guess();
+    CheckWinner();
+  } else {
+    mistakes++;
+    document.getElementById("mistakes").innerHTML = mistakes;
+  }
+  document.getElementById(letter).setAttribute("disabled", true);
+  CheckWinner();
+  console.log("You pressed:", letter);
+};
+document.getElementById("maxWrongGuesse").innerHTML = maxWrong;
